46c758337848965d4e36dd1da545619814e32348
João Pedro <jpedrogs02@gmail.com>
2025-05-28 18:58:00 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -1,6 +1,6 @@
 public class Livro extends Produto {
     private String autor;
-
+// Construtor: usa super() para inicializar a superclasse Produto
     public Livro(int id, String nome, double preco, String autor) {
         super(id, nome, preco);
         this.autor = autor;
